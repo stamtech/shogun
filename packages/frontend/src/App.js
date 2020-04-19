d165fce8a3f2dde2a23e3edb5adab05b60c83ffd
Sofiane Stamboul <sofiane_stamboul.external@discovery.com>
2020-04-20 00:06:50 +0200

    add page tu create or join a game

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Image, Flex, Button, Input, Box } from "@chakra-ui/core";
+import { Text } from "@chakra-ui/core";
+import Samurai from "./img/samurai.jpg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Text fontSize='6xl' textAlign='center'>
+        Katana
+      </Text>
+      <Flex align='center' justify='center'>
+        <Box size='md'>
+          <Image src={Samurai} alt='Segun Adebayo' />
+        </Box>
+      </Flex>
+      <Flex align='center' justify='center'>
+        <Box marginBottom='20px'>
+          <Button variantColor='teal'>Créer une partie </Button>
+        </Box>
+      </Flex>
+      <Flex align='center' justify='center'>
+        <Flex>
+          <Button marginRight='10px'>Rejoindre une partie </Button>
+          <Input width='230px' placeholder='Entrez un code pour rejoindre une partie' />
+        </Flex>
+      </Flex>
     </div>
   );
 }
